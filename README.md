3f55e9003862a72e9ac998f21ac827f57243edb2
Dvwhite81 <Dvwhite1981@gmail.com>
2023-09-14 11:40:01 -0400

    Refactor with buttons - Complete

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Odin_rock-paper-scissors
+
+Button images credit:
+<a href="https://www.vecteezy.com/free-vector/rock-paper-scissors">Rock Paper Scissors Vectors by Vecteezy</a>
+
+Background image by <a href="https://www.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_13223496.htm#query=background&position=0&from_view=keyword&track=sph">Freepik</a>
--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-const prompt = require("prompt-sync")({ sigint: true });
 const choices = ["rock", "paper", "scissors"];
+const choiceButtons = document.querySelectorAll(".choice-button");
+
+const resultsDisplay = document.querySelector(".results-display");
+
+const playerScoreSpan = document.getElementById("player-score");
+let scorePlayer = parseInt(playerScoreSpan.innerHTML);
+const computerScoreSpan = document.getElementById("computer-score");
+let scoreComputer = parseInt(computerScoreSpan.innerHTML);
+const tieScoreSpan = document.getElementById("tie-score");
+let scoreTie = parseInt(tieScoreSpan.innerHTML);
+
+const resetButton = document.getElementById("reset-button");
 
 function getComputerChoice() {
   const choice = choices[Math.floor(Math.random() * choices.length)];
   return choice;
 }
 
-function getPlayerChoice() {
-    let answer = "answer";
-
-    while (!choices.includes(answer)) {
-      answer = prompt("Choose Rock, Paper, or Scissors: ").toLowerCase();
-    }
-
-    let newAnswer = capitalize(answer);
-
-    return newAnswer;
-}
-
 function capitalize(str) {
-    let first = str.charAt(0);
-    first = first.toUpperCase();
-    const letters = str.slice(1);
-    let newStr = first + letters;
+  let first = str.charAt(0);
+  first = first.toUpperCase();
+  const letters = str.slice(1);
+  let newStr = first + letters;
 
-    return newStr;
+  return newStr;
 }
 
-function playRound() {
-  const player = getPlayerChoice();
+function playRound(player) {
   const computer = capitalize(getComputerChoice());
+  player = capitalize(player);
 
   let winner;
   if (
@@ -37,43 +36,65 @@ function playRound() {
     (player === "Scissors" && computer === "Paper") ||
     (player === "Paper" && computer === "Rock")
   ) {
-    console.log(`You win! ${player} beats ${computer}!`);
+    resultsDisplay.innerHTML = `You win! ${player} beats ${computer}!`;
     winner = 1;
+    playerScoreSpan.innerHTML = scorePlayer += 1;
+    checkForWin();
   } else if (
     (computer === "Rock" && player === "Scissors") ||
     (computer === "Scissors" && player === "Paper") ||
     (computer === "Paper" && player === "Rock")
   ) {
-    console.log(`You lose! ${computer} beats ${player}`);
+    resultsDisplay.innerHTML = `You lose! ${computer} beats ${player}`;
     winner = -1;
+    computerScoreSpan.innerHTML = scoreComputer += 1;
+    checkForWin();
   } else {
-    console.log(`It's a tie! ${player} ties with ${computer}`);
-    winner = 0;
+    resultsDisplay.innerHTML = `It's a tie! ${player} ties with ${computer}`;
+    tieScoreSpan.innerHTML = scoreTie += 1;
   }
 
   return winner;
 }
 
-function game() {
-  let player = 0;
-  let computer = 0;
-  let tie = 0;
-  let results = '';
-
-  for (let i = 0; i < 5; i++) {
-    let winner = playRound();
-    if (winner === 1) player++;
-    else if (winner === -1) computer++;
-    else tie++;
-  }
-
-  if (player > computer) results += 'You won the series! ';
-  else if (computer > player) results += 'You lost the series! ';
-  else results += 'The series was a tie! ';
-
-  results += `Final score - You: ${player}  Computer: ${computer}  Ties: ${tie}`;
-
-  return results;
+function startGame(e) {
+  const player = e.target.getAttribute("data-choice");
+  playRound(player);
 }
 
-console.log(game());
+function addListeners() {
+  choiceButtons.forEach((button) => {
+    button.addEventListener("click", startGame);
+  });
+  resetButton.addEventListener("click", resetGame);
+}
+
+function checkForWin() {
+  if (scorePlayer === 5) {
+    endGame("You won the series!");
+  } else if (scoreComputer === 5) {
+    endGame("You lost the series!");
+  }
+}
+
+function endGame(message) {
+  choiceButtons.forEach((button) => {
+    button.removeEventListener("click", startGame);
+  });
+
+  alert(message);
+}
+
+function resetGame() {
+  scorePlayer = 0;
+  scoreComputer = 0;
+  scoreTie = 0;
+  playerScoreSpan.innerHTML = scorePlayer;
+  computerScoreSpan.innerHTML = scoreComputer;
+  tieScoreSpan.innerHTML = scoreTie;
+  resultsDisplay.innerHTML = "";
+
+  addListeners();
+}
+
+resetGame();
